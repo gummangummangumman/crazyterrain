605e03010086eb3225a8657a43ad0063899fbc28
fhqwhgads <bendik_b2@hotmail.com>
2017-10-30 15:48:22 +0100

    sneky

--- shaders/fshader.glsl
+++ shaders/fshader.glsl
@@ -1,6 +1,6 @@
 // Other attributes and uniforms are supposed to be filled in by Three.js
 // To avoid it being filled in automatically, use RawShaderMaterial
-
+// Sneaky test
 uniform vec3 color;
 
 void main()
